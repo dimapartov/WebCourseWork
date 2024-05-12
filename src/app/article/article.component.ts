55241bfd11cd32c53dbcf03aa8dacc08abbc61de
dimapartov <mr_kubarev@mail.ru>
2024-05-12 20:29:19 +0300

    Add article component

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.scss',
+})
+export class ArticleComponent {
+  @Input() article = {
+    title: 'Article Title',
+    category: 'Article Category',
+    author: 'Article Author',
+    content: 'Article Content',
+  };
+  @Output() deleteArticle = new EventEmitter<void>();
+  @Output() archiveArticle = new EventEmitter<void>();
+  onDelete() {
+    this.deleteArticle.emit();
+  }
+  onArchive() {
+    this.archiveArticle.emit();
+  }
+}
